533deb953ce86a4401cbcf28100cf6a7826b90d8
Darsika-Thavasingam <darsikadarsi80@gmail.com>
2025-04-23 20:33:53 +0530

    Initial commit / ProfileEdit page added

--- frontend/src/pages/profile/ProfileEdit.jsx
+++ frontend/src/pages/profile/ProfileEdit.jsx
@@ -1,13 +1,41 @@
 // src/pages/profile/ProfileEdit.jsx
-import React from 'react';
-
-function ProfileEdit() {
+import React from "react";
+import '../../styles/ProfileEdit/ProfileEdit.css';
+import profileAvatar from "../../resources/images/ProfileEdit/profile.jpg"
+const ProfileEdit = () => {
   return (
-    <div>
-      <h2>Profile Edit Page</h2>
-      <p>This is the Profile Edit page for Paddy Platform.</p>
+    <div
+      className="page-container">
+      <div className="form-container">
+        <h2 className="form-title">Profile</h2>
+
+        <div className="avatar-section">
+          <img
+             src={profileAvatar}
+            alt="Avatar"
+            className="avatar-image"
+          />
+          <span className="status-text" >Change</span>
+        </div>
+
+        <form className="profile-form">
+          <input className="form-input" type="text" placeholder="Name" />
+          <input className="form-input" type="text" placeholder="Profile Type" />
+          <textarea className="form-input" placeholder="Description" />
+          <input className="form-input" type="text" placeholder="Province" />
+          <input className="form-input" type="email" placeholder="Email" />
+          <input className="form-input" type="tel" placeholder="Phone" />
+
+          <button
+            type="submit"
+            className="save-button"
+          >
+            Save
+          </button>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default ProfileEdit;
