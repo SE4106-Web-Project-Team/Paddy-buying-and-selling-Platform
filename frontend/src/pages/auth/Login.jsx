b9b89f7718ce56142b267fb2c752e0c9ae43f62a
isuru0 <isurushehara@gmail.com>
2025-04-23 19:06:57 +0530

    Structure and style the login page

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,11 +1,20 @@
 // src/pages/Login.jsx
 import React from 'react';
+import '../../styles/auth/login.css';
 
 function Login() {
   return (
-    <div>
-      <h2>Login Page</h2>
-      <p>This is the login page for Paddy Platform.</p>
+    <div className="login-container">
+      <div class="login-container-blur-layer"></div>
+      <div className="login-card">
+        <h2>Login</h2>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Login</button>
+        <hr />
+        <p>Forgot? <a href="/signup">Password?</a></p>
+        <p>Don't have an account? <a href="/signup">Sign up</a></p>
+      </div>
     </div>
   );
 }
