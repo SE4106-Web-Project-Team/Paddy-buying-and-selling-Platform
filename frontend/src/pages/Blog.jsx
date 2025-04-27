d157afd9f12ac59de67fe31d35bdbfeada010ae8
isuru0 <isurushehara@gmail.com>
2025-04-27 15:27:41 +0530

    Link the blog page

---
+++ frontend/src/pages/Blog.jsx
@@ -0,0 +1,13 @@
+// src/pages//Blog.jsx
+import React from 'react';
+
+function Blog() {
+  return (
+    <div>
+      <h2>Blog Page</h2>
+      <p>This is the Blog page for Paddy Platform.</p>
+    </div>
+  );
+}
+
+export default Blog;
