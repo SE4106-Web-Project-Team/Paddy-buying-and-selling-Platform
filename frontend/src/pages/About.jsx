347c7f0c48cd406b541d885d0b95fd59d95d341b
Bhagya Piyumali <bhagyapiyumali2001@gmail.com>
2025-05-21 21:06:18 +0530

    create dash board

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,18 +1,26 @@
 // src/pages/About.jsx
 import React from "react";
+import "../styles/about/about.css";
 import { Link, useNavigate } from "react-router-dom";
 import NavigationBar from "../components/nav/NavigationBar";
-import "../styles/about/about.css";
+
 
 function About() {
   return (
     <div>
       <NavigationBar />
       <p>
-        <a href="/">Back</a>
+        <a href="/" className="back-button">Back</a>
+
+
       </p>
-      <h2 className="hh">About Page</h2>
+      <div className="about-container">
+        <h1>About Page</h1>
       <p>This is the About page for Paddy Platform.</p>
+      <p>
+        Welcome to our paddy field platform, where tradition meets innovation. Rooted in Sri Lanka's rich agricultural heritage, we aim to empower farmers and connect them with modern tools and markets. Our mission is to enhance sustainable rice cultivation by providing resources, knowledge, and a digital marketplace that benefits every stakeholder in the paddy ecosystem. Join us in cultivating a prosperous future for our farming communities..
+      </p>
+      </div>
     </div>
   );
 }
