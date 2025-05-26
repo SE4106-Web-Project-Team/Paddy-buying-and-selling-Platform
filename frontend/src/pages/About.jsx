b828b5be1ec94af21a0903df3bc745e1465ff5a5
isuru0 <isurushehara@gmail.com>
2025-05-26 18:48:01 +0530

    Fix a conflic

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -10,18 +10,27 @@ function About() {
     <div>
       <NavigationBar />
       <p>
-        <a href="/" className="back-button">Back</a>
-
-
+        <a href="/" className="back-button">
+          Back
+        </a>
       </p>
-      <div className="about-image" style={{ backgroundImage: `url(${aboutImage})` }}>
-      <div className="about-container">
-        <h1>About Page</h1>
-      <p>This is the About page for Paddy Platform.</p>
-      <p>
-        Welcome to our paddy field platform, where tradition meets innovation. Rooted in Sri Lanka's rich agricultural heritage, we aim to empower farmers and connect them with modern tools and markets. Our mission is to enhance sustainable rice cultivation by providing resources, knowledge, and a digital marketplace that benefits every stakeholder in the paddy ecosystem. Join us in cultivating a prosperous future for our farming communities..
-      </p>
-      </div>
+      <div
+        className="about-image"
+        style={{ backgroundImage: `url(${aboutImage})` }}
+      >
+        <div className="about-container">
+          <h1>About Page</h1>
+          <p>This is the About page for Paddy Platform.</p>
+          <p>
+            Welcome to our paddy field platform, where tradition meets
+            innovation. Rooted in Sri Lanka's rich agricultural heritage, we aim
+            to empower farmers and connect them with modern tools and markets.
+            Our mission is to enhance sustainable rice cultivation by providing
+            resources, knowledge, and a digital marketplace that benefits every
+            stakeholder in the paddy ecosystem. Join us in cultivating a
+            prosperous future for our farming communities..
+          </p>
+        </div>
       </div>
     </div>
   );
